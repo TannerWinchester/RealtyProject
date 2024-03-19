bac54129debe521d46cabd9c5748fb17ace001a0
Tanner Winchester <tannerwinchester@Tanners-MBP.home>
2024-03-18 23:59:37 -0600

    exploring json data sets

--- API.py
+++ API.py
@@ -9,22 +9,18 @@
 
 import requests
 import pandas as pd
+from pandas import json_normalize
 
-home_values = pd.read_csv('data/HomeValues.csv')
-df_home_values = pd.DataFrame(home_values)
-# print(df_home_values.head())
-
-# clean the data
-clean_df_home_values = df_home_values.dropna()
-pd.options.display.float_format = '{:,.2f}'.format
-print(clean_df_home_values.head(30))
-
-print(f'df shape: {clean_df_home_values.shape}')
-
-
-
-
-
+# home_values = pd.read_csv('data/HomeValues.csv')
+# df_home_values = pd.DataFrame(home_values)
+# # print(df_home_values.head())
+#
+# # clean the data
+# clean_df_home_values = df_home_values.dropna()
+# pd.options.display.float_format = '{:,.2f}'.format
+# print(clean_df_home_values.head(30))
+#
+# print(f'df shape: {clean_df_home_values.shape}')
 
 # if we get access to the API from zillow
 # endpoint = 'GET https://rentalsapi.zillowgroup.com/listings/v1/listingsForUser'
@@ -40,3 +36,28 @@ print(f'df shape: {clean_df_home_values.shape}')
 #     'near': city,
 #     'api_key': api_key
 # }
+
+import requests
+
+endpoint = "https://api.schooldigger.com/v2.0/rankings/schools/{st}"
+
+
+def school_search():
+    params = {
+        "q": "Boise",
+        "st": "ID",
+        "appID": "f607f499",
+        "appKey": "da66c0739308a6fef6f4a4e37d0a2c55"
+    }
+
+    res = requests.get(endpoint, params=params)
+    school_data = res.json()
+    print(school_data['schoolList'][0])
+
+    # flattened_data = json_normalize(school_data)
+    #
+    # df_schools = pd.DataFrame(flattened_data)
+    # print(df_schools.head())
+
+
+school_search()
