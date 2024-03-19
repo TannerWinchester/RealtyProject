2a238ba9fd62fd2bb5f234922470370bf5a82a94
hunter <hunter411w@gmail.com>
2024-03-18 18:26:47 -0600

    Minor edits to html templates, tinkering around with school API

--- API.py
+++ API.py
@@ -1,1 +1,18 @@
 # Tanner will start API work
+
+
+
+import requests
+link = "https://api.schooldigger.com/v2.0/rankings/schools/{st}"
+
+def school_search():
+    params = {
+        "q": "Boise",
+        "st": "ID",
+        "appID": "f607f499",
+        "appKey": "da66c0739308a6fef6f4a4e37d0a2c55"
+    }
+    query = requests.get(link, params=params)
+    print(query.json())
+
+school_search()
